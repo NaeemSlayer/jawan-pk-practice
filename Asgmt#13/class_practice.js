8a1f34f9474e0ec22a11045b03ccef0f15869669
Muhammad Naeemuddin <muhammedalam150@gmail.com>
2024-09-08 18:48:24 +0500

    assignment 13

---
+++ Asgmt#13/class_practice.js
@@ -0,0 +1,88 @@
+// // chapter 1
+// // q1
+// alert("Greetings User")
+// // q2
+// alert("Error! Please enter a valid password")
+// // q3
+// alert("Welcome to JS land...\nHappy Codding")
+// // q4
+// alert("Welcome to JS land...")
+// alert("Happy Codding")
+// //q5
+// alert("hello..i can run JS through my web browser's console")
+
+// // chapter 2
+// // q1
+// let username
+// // q2
+// let myName = "Naeem Alam"
+// //q3
+// let message = "Hello World"
+// alert(message)
+// // q4
+// let stdname = "Jhone Doe"
+// let stdage = "15 years old"
+// let stdqual = "Certified Mobile Application Development"
+// alert(stdname)
+// alert(stdage)
+// alert(stdqual)
+// // q5
+// let pizzaTree = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizzaTree)
+// // q6
+// let email = "example@example.com"
+// alert("My email address is " + email)
+// // q7
+// let book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book" + book)
+// // q8
+// document.write("Yeah! I can write HTML content through JavaScript")
+// // q9
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// // chapter 3
+// // q1
+// let myage = 24;
+// alert(I am ${myage} years old);
+
+// // q2 
+// let visits = 14;
+// alert("You have visited the site " + visits + " times");
+
+// // q3
+// var year = 2002;
+// document.write(My birth year is ${year} <br>Data type of my declared variable is ${typeof year}<br>);
+
+// // q4
+// let visitorName = "John Doe";
+// let productTitle = "5 T-shirt(s)";
+// let quantity = "XYZ Clothing Store";
+// document.write(${visitorName} ordered ${productTitle} on ${quantity}<br>);
+
+
+
+// // chapter 4
+// // q1 
+// let x = 10, y = 20, z = 30;
+
+// // q2
+// //legal
+// var userName;
+// var _age;
+// var $price;
+// var first_name;
+// var total5;
+
+// //illegal
+// // 2ndPlace
+// // let
+// // user-name
+// // @data
+// // var!
+
+// // q3
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("<p>Variable names can only contain letters, digits, underscores, and dollar signs. For example $my_1stVariable</p>");
+// document.write("<p>Variables must begin with a letter, dollar sign ($), or underscore (_). For example $name, _name, or name</p>");
+// document.write("<p>Variable names are case sensitive</p>");
+// document.write("<p>Variable names should not be JS keywords</p>");
---
+++ Asgmt#13/homework.js
@@ -0,0 +1,41 @@
+// Chapter 1 (Alerts)
+alert("First Name");
+alert("Last Name");
+alert("Email");
+alert("Phone Number");
+alert("Password");
+alert("You're learning JavaScript!");
+alert("I enjoy learning JavaScript!");
+
+// Chapter 2 (Variables for string)
+let userOneId;
+
+let emptyVariable;
+emptyVariable = "Not Empty Anymore"
+
+const teamName = "AlphaNumerade"
+alert(teamName)
+
+var bestMan = "Charlie"
+bestMan = "Harry"
+alert(bestMan)
+
+// Chapter 3 (Variables for numbers)
+let caseQty
+
+caseQty = 144
+alert(caseQty)
+
+var num = 9
+
+var twoNums
+twoNums = 2+2
+alert(twoNums)
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+alert(orderTotal)
+
+var addNum = 9
+alert(addNum+1)
